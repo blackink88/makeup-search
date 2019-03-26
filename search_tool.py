40e3c57344959e290a2d34c8b68451346f6fda33
Sam <ktnpop001@myuct.ac.za>
2019-03-26 12:46:15 +0200

    Makeup information tool
    Using an API to search for certain brands of makeup.

--- search_tool.py
+++ search_tool.py
@@ -1,13 +1,12 @@
 import urllib.request as request
 import json
-
+#User input
 search_product = input("Search for your makeup brand: ")
-
+#Request the data needed to be read
 with request.urlopen('http://makeup-api.herokuapp.com/api/v1/products.json') as response:
   source = response.read()
 data = json.loads(source)
-
-
+#Search for the brand of makeup
 if search_product in data  == True:
  products = data['brand']    
  d = {v[search_product]: h for h, v in products.items()}

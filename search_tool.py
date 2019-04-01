fa8c0cb336c5bf2787cb97ed2e68460c220f0f0b
Sam <ktnpop001@myuct.ac.za>
2019-04-01 09:05:00 +0200

    Update

--- search_tool.py
+++ search_tool.py
@@ -1,15 +1,10 @@
 import urllib.request
 import json
 #User input
-
-search_brand = input("Enter value: ")
+search_brand = input("Enter the brand: ")
 #Request the data needed to be read
 with urllib.request.urlopen("http://makeup-api.herokuapp.com/api/v1/products.json") as url:
     data = json.loads(url.read().decode())
-
-
-
 for brands in data:
-
  d = [k for k,v in brands.items() if v == search_brand]
  print(d)

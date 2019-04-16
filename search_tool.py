61a17b55427348e96f2a8e4cf90409d5d6f34b42
Sam <ktnpop001@myuct.ac.za>
2019-04-16 12:58:50 +0200

    Update search_tool.py

--- search_tool.py
+++ search_tool.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 #User input
-search_input = 'fenty'
+search_input = input("Search for your makeup brand: ")
 #Request the data needed to be read
 with urllib.request.urlopen("http://makeup-api.herokuapp.com/api/v1/products.json") as url:
     data = json.loads(url.read().decode())
@@ -9,5 +9,7 @@ with urllib.request.urlopen("http://makeup-api.herokuapp.com/api/v1/products.jso
 for values in data:
  if values['brand'] == search_input:
   print(values['brand'])
+  print(values['name'])
+
 
 
